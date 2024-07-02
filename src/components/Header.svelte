c2705121c6cae2b2fb1512d7973ffe5e14cdeadb
Sean Kim <sean.kim0327@gmail.com>
2024-07-01 23:40:42 -0400

    header is more mobile friendly

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,29 +1,21 @@
-<header class="flex flex-row justify-between font-josefin-slab mt-4 absolute w-full">
-    <div class="mx-4 sm:mx-16 font-semibold text-md sm:text-xl">
-        <a href="/">
-            skarokin
-        </a>
+<header
+    class="flex flex-row justify-between font-josefin-slab mt-4 absolute w-full"
+>
+    <div class="mx-4 sm:mx-16 font-semibold text-base sm:text-xl skarokin">
+        <a href="/"> skarokin </a>
     </div>
-    <ul class="flex font-semibold text-md sm:text-xl">
+    <ul class="flex font-semibold text-base sm:text-xl">
         <li class="mr-4 sm:mr-16">
-            <a href="/projects">
-                projects
-            </a>
+            <a href="/projects"> projects </a>
         </li>
         <li class="mr-4 sm:mr-16">
-            <a href="/contacts">
-                contacts
-            </a>
+            <a href="/contacts"> contacts </a>
         </li>
         <li class="mr-4 sm:mr-16">
-            <a href="/about">
-                about
-            </a>
+            <a href="/about"> about </a>
         </li>
         <li class="mr-4 sm:mr-16">
-            <a href="/resume">
-                resume
-            </a>
+            <a href="/resume"> resume </a>
         </li>
     </ul>
 </header>
@@ -43,21 +35,63 @@
 
     @media (max-width: 640px) {
         li {
-            padding-left: 0.25rem;
+            padding-left: 0.15rem;
         }
 
         li:hover {
             transform: scale(1.05);
         }
+
+        .skarokin a {
+            font-size: 0;
+            display: inline-block;
+        }
+
+        .skarokin a::before {
+            content: "<";
+            font-size: 1.5rem;
+            color: var(--text-main);
+        }
+
+        .skarokin a:active::before {
+            color: var(--peach);
+        }
     }
 
     li a {
         text-decoration: none;
-        color: #4c4f69;
+        color: var(--text-main);
     }
 
-    
     li a:hover {
         color: var(--peach);
+    }
+
+    .skarokin {
+        transition: all 0.2s ease;
+    }
+
+    .skarokin:hover {
+        color: var(--peach);
+        transform: scale(1.1);
+    }
+
+    @media (min-width: 641px) {
+        .skarokin::before {
+            content: "<";
+            color: var(--text-main);
+            position: absolute;
+            transform: translate(0, 0);
+            display: inline-block;
+            transform-origin: center;
+            opacity: 0;
+            transition:
+                opacity 0.1s ease,
+                transform 0.2s ease;
+        }
+        .skarokin:hover::before {
+            opacity: 1;
+            transform: translate(-1em, 0);
+        }
     }
 </style>
